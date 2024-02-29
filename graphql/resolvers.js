2af1d27774814da5d04ee02c718aaf28dd5d124b
Julian <jscristancho83@gmail.com>
2024-02-29 09:29:37 -0500

    solucion al bug de graphql sobre los parametros

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,12 @@
 const UserSchema = require('../models/User')
+const HouseSchema = require('../models/Houses')
 
 const resolvers = {
     hello: () => {
         return "hola mundo";
     },
-    User: async ({id}) => {
+    User: async (_, { id }) => {
+        console.log({ id }, id);
         try {
             return user = await UserSchema.findById(id)
         } catch (error) {
@@ -18,25 +20,86 @@ const resolvers = {
             console.log(error)
         }
     },
-    UsersByFilter: async ({filter}) =>{
+    UsersByFilter: async (_, { filter }) => {
         try {
 
             let query = {}
 
-            if(filter){
-                if(filter.name){
-                    query.name = {$regex: filter.name, $options: 'i'}
+            if (filter) {
+                if (filter.name) {
+                    query.name = { $regex: filter.name, $options: 'i' }
                 }
-                if(filter.email){
-                    query.email = {$regex: filter.email, $options: 'i'}
+                if (filter.email) {
+                    query.email = { $regex: filter.email, $options: 'i' }
                 }
-                if(filter.lastname){
-                    query.lastname = {$regex: filter.lastname, $options: 'i'}
+                if (filter.lastname) {
+                    query.lastname = { $regex: filter.lastname, $options: 'i' }
                 }
 
                 const users = await UserSchema.find(query)
                 return users;
-            } 
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    House: async (_, { id }) => {
+        try {
+            return user = await HouseSchema.findById(id)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    Houses: async () => {
+        try {
+            return await HouseSchema.find();
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    HouseByFilter: async (_, { filter }) => {
+        try {
+
+            let query = {}
+
+            if (filter) {
+                if (filter.address) {
+                    query.address = { $regex: filter.address, $options: 'i' }
+                }
+                if (filter.city) {
+                    query.city = { $regex: filter.city, $options: 'i' }
+                }
+                if (filter.state) {
+                    query.state = { $regex: filter.state, $options: 'i' }
+                }
+                if (filter.size) {
+                    query.size = { $regex: filter.size, $options: 'i' }
+                }
+                if (filter.type) {
+                    query.type = { $regex: filter.type, $options: 'i' }
+                }
+                if (filter.zipCode) {
+                    query.zipCode = { $regex: filter.zipCode, $options: 'i' }
+                }
+                if (filter.code) {
+                    query.code = { $regex: filter.code, $options: 'i' }
+                }
+                if (filter.rooms) {
+                    query.rooms = { $regex: filter.rooms, $options: 'i' }
+                }
+                if (filter.bathrooms) {
+                    query.bathrooms = { $regex: filter.bathrooms, $options: 'i' }
+                }
+                if (filter.price) {
+                    query.price = { $regex: filter.price, $options: 'i' }
+                }
+                if (filter.image) {
+                    query.image = { $regex: filter.image, $options: 'i' }
+                }
+
+                const houses = await HouseSchema.find(query)
+                return houses;
+            }
         } catch (error) {
             console.log(error);
         }
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,103 +1,123 @@
-// const { buildSchema } = require('graphql')
-
-// const schema = buildSchema(`
-//     type: User {
-//         id: ID!
-//         name: String!
-//         lastname: String!
-//         email: String!
-//         avatar: String!
-//     }
-//     type: Message {
-//         id: ID!
-//         body: String!
-//         from: User!
-//         to: User!
-//         readed: Boolean
-//     }
-//     type: House {
-//         id: ID!
-//         address: String!
-//         city: String!
-//         state: String!
-//         size: Int!
-//         type: String!
-//         zip_code: String!
-//         code: String!
-//         rooms: Int!
-//         bathrooms: Int!
-//         price: Int!
-//         image: String!
-//     }`)
-
-// module.exports = schema
-
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLBoolean, GraphQLInputObjectType, GraphQLList, GraphQLSchema } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLBoolean, GraphQLInputObjectType, GraphQLList, GraphQLSchema, GraphQLInt } = require('graphql');
 const resolvers = require('./resolvers');
 
 const User = new GraphQLObjectType({
-    name: 'User',
-    fields: {
-        _id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        lastname: {type: GraphQLString},
-        email: {type: GraphQLString},
-        avatar: {type: GraphQLString},
-    }
+  name: 'User',
+  fields: {
+    _id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    lastname: { type: GraphQLString },
+    email: { type: GraphQLString },
+    avatar: { type: GraphQLString },
+  }
 })
 
 const Message = new GraphQLObjectType({
-    name: 'Message',
-    fields: {
-        _id: {type: GraphQLID},
-        body: {type: GraphQLString},
-        from: {type: User},
-        to: {type: User},
-        readed: {type: GraphQLBoolean},
-    }
+  name: 'Message',
+  fields: {
+    _id: { type: GraphQLID },
+    body: { type: GraphQLString },
+    from: { type: User },
+    to: { type: User },
+    readed: { type: GraphQLBoolean },
+  }
+})
+
+const House = new GraphQLObjectType({
+  name: 'House',
+  fields: {
+    _id: { type: GraphQLID },
+    address: { type: GraphQLString },
+    city: { type: GraphQLString },
+    state: { type: GraphQLString },
+    size: { type: GraphQLInt },
+    type: { type: GraphQLString },
+    zipCode: { type: GraphQLInt },
+    code: { type: GraphQLString },
+    rooms: { type: GraphQLInt },
+    bathrooms: { type: GraphQLInt },
+    price: { type: GraphQLInt },
+    image: { type: GraphQLString }
+  }
+})
+
+const HouseFilterInput = new GraphQLInputObjectType({
+  name: 'HouseFilterInput',
+  fields: {
+    _id: { type: GraphQLID },
+    address: { type: GraphQLString },
+    city: { type: GraphQLString },
+    state: { type: GraphQLString },
+    size: { type: GraphQLInt },
+    type: { type: GraphQLString },
+    zipCode: { type: GraphQLInt },
+    code: { type: GraphQLString },
+    rooms: { type: GraphQLInt },
+    bathrooms: { type: GraphQLInt },
+    price: { type: GraphQLInt },
+    image: { type: GraphQLString }
+  }
 })
 
 const UserFilterInput = new GraphQLInputObjectType({
-    name: 'UserFilterInput',
-    fields: {
-        name: {type: GraphQLString},
-        lastname: {type: GraphQLString},
-        email: {type: GraphQLString},
-    }
+  name: 'UserFilterInput',
+  fields: {
+    name: { type: GraphQLString },
+    lastname: { type: GraphQLString },
+    email: { type: GraphQLString },
+  }
 })
 
 const queries = {
-    hello: {
-      type: GraphQLString, // Tipo de respuesta
-      resolve: resolvers.hello
-    },
-    User: {
-      type: User,
-      resolve: resolvers.User,
-      args: {
-        id: {type: GraphQLString}
-      }
-    },
-    Users: {
-      type: GraphQLList(User),
-      resolve: resolvers.Users
-    },
-    UsersByFilter: {
-      type: GraphQLList(User),
-      resolve: resolvers.UsersByFilter,
-      args: {
-        filter: { type: UserFilterInput }
-      }
+  hello: {
+    type: GraphQLString, // Tipo de respuesta
+    resolve: resolvers.hello
+  },
+  User: {
+    type: User,
+    resolve: resolvers.User,
+    args: {
+      id: { type: GraphQLString }
+    }
+  },
+  Users: {
+    type: GraphQLList(User),
+    resolve: resolvers.Users
+  },
+  UsersByFilter: {
+    type: GraphQLList(User),
+    resolve: resolvers.UsersByFilter,
+    args: {
+      filter: { type: UserFilterInput }
+    }
+  },
+  House: {
+    type: House,
+    resolve: resolvers.House,
+    args: {
+      id: { type: GraphQLString }
+    }
+  },
+  Houses: {
+    type: new GraphQLList(House),
+    resolve: resolvers.Houses
+  },
+  HouseByFilter: {
+    type: new GraphQLList(House),
+    resolve: resolvers.HouseByFilter,
+    args: {
+      filter: { type: HouseFilterInput }
     }
   }
-  
-  const queryType = new GraphQLObjectType({
-    name: 'Query',
-    fields: queries
-  })
-  
-  const schema = new GraphQLSchema({
-    query: queryType
-  })
-  
-  module.exports = schema
+}
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: queries
+})
+
+const schema = new GraphQLSchema({
+  query: queryType
+})
+
+module.exports = schema
